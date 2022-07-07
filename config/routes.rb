3fe7689cb22e86dc1b69795c91eaa51b660a382a
rana haseeb <alix@yahoo.com>
2022-07-07 12:14:20 +0500

    the query string in non-resourceful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
- get ':controller/:action/:id/with_user/:user_id'
+ get ':controller/:action/:id'
 
  # perfoming crud operations for running functions according to our need in controller may be defined at individual level  if we we are not using restful routing as line of code mentioned above.
 
